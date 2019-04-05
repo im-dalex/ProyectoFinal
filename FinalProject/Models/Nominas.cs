8e9fe3a225cf1946672d87f6c114f3c90853c15d
space-runner <dalex2525@hotmail.com>
2019-04-05 14:42:35 -0400

    Agregar archivos de proyecto.

---
+++ FinalProject/Models/Nominas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal.Models
+{
+    public class Nominas
+    {
+        public int ID { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Ano { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Mes { get; set; }
+        
+        public int montoTotal { get; set; }
+    }
+}
